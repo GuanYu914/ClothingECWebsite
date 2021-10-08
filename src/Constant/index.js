f5c0fe2057c0c255c3845173474c5d3daf26c4e1
Emory <aa774281@gmail.com>
2021-10-08 21:36:16 +0800

    add constant to manage components easily

---
+++ src/Constant/index.js
@@ -0,0 +1,11 @@
+// Component Header 相關常數
+export const HEADER_HEIGHT_OFFSET = "4rem";
+
+// Z-Index 相關常數
+// 利用等級區分，方便之後管理
+export const Z_INDEX_LV1 = "9";
+export const Z_INDEX_LV2 = "99";
+export const Z_INDEX_LV3 = "999";
+export const Z_INDEX_LV4 = "9999";
+export const Z_INDEX_LV5 = "99999";
+export const Z_INDEX_LV6 = "999999";
