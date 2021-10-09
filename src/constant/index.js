106b43f107376a09d5800cf1120094c0a27aa2b3
Emory <aa774281@gmail.com>
2021-10-09 19:20:22 +0800

    add break points vars

--- src/constant/index.js
+++ src/constant/index.js
@@ -9,3 +9,8 @@ export const Z_INDEX_LV3 = "999";
 export const Z_INDEX_LV4 = "9999";
 export const Z_INDEX_LV5 = "99999";
 export const Z_INDEX_LV6 = "999999";
+
+// 定義斷點，如果要修改數值，也請修改 src/utils/fonts.css 檔案
+export const BREAKPOINT_MOBILE = "@media (min-width: 320px)";
+export const BREAKPOINT_PAD = "@media (min-width: 768px)";
+export const BREAKPOINT_LAPTOP = "@media (min-width: 1440px)";
