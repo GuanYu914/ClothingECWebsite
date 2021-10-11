b614669e98fed7299d125ac186f0a2d64d28e022
Emory <aa774281@gmail.com>
2021-10-11 11:21:44 +0800

    add container style on pad size and laptop size

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,15 @@
 import { ReactComponent as logo } from "../../imgs/components/header/bootstrap.svg";
 import { ReactComponent as list } from "../../imgs/components/header/list.svg";
+import { ReactComponent as profile } from "../../imgs/components/header/person-circle.svg";
+import { ReactComponent as shopping_bag } from "../../imgs/components/header/bag.svg";
 import styled from "styled-components";
-import { HEADER_HEIGHT_OFFSET, Z_INDEX_LV6 } from "../../constant";
-
-const Container = styled.div``;
+import {
+  BREAKPOINT_LAPTOP,
+  BREAKPOINT_MOBILE,
+  BREAKPOINT_PAD,
+  HEADER_HEIGHT_OFFSET,
+  Z_INDEX_LV6,
+} from "../../constant";
 
 const NavBarContainer = styled.nav.attrs(() => ({
   className: "bg-secondary3",
@@ -19,28 +25,94 @@ const NavBarContainer = styled.nav.attrs(() => ({
   justify-content: space-between;
   align-items: center;
   box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
+
+  ${BREAKPOINT_MOBILE} {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  ${BREAKPOINT_PAD} {
+    height: 5.6rem;
+  }
+
+  // 裝置尺寸為 1440px 寬時，將 NavBar 固定為 1440px 寬
+  ${BREAKPOINT_LAPTOP} {
+    padding-left: calc((100% - 90rem) / 2);
+    padding-right: calc((100% - 90rem) / 2);
+  }
 `;
 
 const BrandLogo = styled(logo)`
   width: 2rem;
   height: 2rem;
   cursor: pointer;
-  margin-left: 1rem;
+
+  ${BREAKPOINT_PAD} {
+    width: 2.4rem;
+    height: 2.4rem;
+  }
 `;
 const HamburgerButton = styled(list)`
   width: 2rem;
   height: 2rem;
   cursor: pointer;
-  margin-right: 1rem;
+
+  ${BREAKPOINT_PAD} {
+    display: none;
+  }
+`;
+
+const FeatureContainer = styled.div`
+  display: none;
+
+  ${BREAKPOINT_PAD} {
+    display: flex;
+  }
+`;
+
+const ProfileContainer = styled.div`
+  display: none;
+
+  ${BREAKPOINT_PAD} {
+    display: flex;
+    align-items: center;
+    margin-right: 2rem;
+  }
+`;
+
+const ProfileIcon = styled(profile)`
+  ${BREAKPOINT_PAD} {
+    width: 2.4rem;
+    height: 2.4rem;
+    cursor: pointer;
+    margin-right: 1rem;
+  }
+`;
+
+const UserNickname = styled.h3.attrs(() => ({
+  className: "fs-h3",
+}))``;
+
+const ShoppingBag = styled(shopping_bag)`
+  ${BREAKPOINT_PAD} {
+    width: 2.4rem;
+    height: 2.4rem;
+    cursor: pointer;
+  }
 `;
 
 export default function Header() {
   return (
-    <Container>
-      <NavBarContainer>
-        <BrandLogo />
-        <HamburgerButton />
-      </NavBarContainer>
-    </Container>
+    <NavBarContainer>
+      <BrandLogo />
+      <HamburgerButton />
+      <FeatureContainer>
+        <ProfileContainer>
+          <ProfileIcon />
+          <UserNickname>冠宇</UserNickname>
+        </ProfileContainer>
+        <ShoppingBag />
+      </FeatureContainer>
+    </NavBarContainer>
   );
 }
