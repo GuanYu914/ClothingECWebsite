1a4833a8e9220173ee4b762b9e94ffc097380c63
Emory <aa774281@gmail.com>
2021-10-19 14:14:27 +0800

    add reset css effects, re-order css import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // 添加自定義的 css 檔案
+// 優先性排序先引入的最小，反之最大
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./utils/reset.css";
+import "./utils/effect.css";
 import "./utils/font.css";
 import "./utils/color.css";
 import "./utils/global.css";
-import "./utils/effect.css";
 import App from "./App";
 
 ReactDOM.render(
