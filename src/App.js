fd18e15e948b97e5e2f3a0ab4eed1d75a008f8bf
Emory <aa774281@gmail.com>
2021-11-05 21:01:44 +0800

    add cartContext and exception handler for route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import LoginPage from "./pages/login-page";
 import RegisterPage from "./pages/register-page";
 import ProfileEditPage from "./pages/profile-edit-page";
 import FavoritePage from "./pages/favorite-page";
-import { UserContext } from "./context/UserContext";
+import { UserContext, CartContext } from "./context";
 
 function App() {
   // Call Web API 拿用戶資訊
@@ -28,38 +28,130 @@ function App() {
   const memorizedUser = useMemo(() => {
     return user;
   }, [user]);
+  // 模擬購物車有產品的時候
+  const [cartContext, setCartContext] = useState([
+    {
+      id: 1,
+      pid: 2315,
+      name: "女版針織衫",
+      urls: [
+        { id: 1, src: "https://i.imgur.com/C7SYk0P.jpg", alt: "product_pic" },
+        { id: 2, src: "https://i.imgur.com/Dfav1Yk.jpg", alt: "product_pic" },
+      ],
+      colors: [
+        { id: 1, hexcode: "#ffce30", selected: false },
+        { id: 2, hexcode: "#e83845", selected: false },
+        { id: 3, hexcode: "#e389b9", selected: false },
+        { id: 4, hexcode: "#746ab0", selected: true },
+        { id: 5, hexcode: "#288ba8", selected: false },
+      ],
+      sizes: [
+        { id: 1, name: "XS", selected: false },
+        { id: 2, name: "S", selected: true },
+        { id: 3, name: "M", selected: false },
+        { id: 4, name: "L", selected: false },
+        { id: 5, name: "XL", selected: false },
+        { id: 6, name: "2L", selected: false },
+      ],
+      quantity: 1,
+      unitPrice: 490,
+    },
+    {
+      id: 2,
+      pid: 1574,
+      name: "男版針織衫",
+      urls: [
+        { id: 1, src: "https://i.imgur.com/C7SYk0P.jpg", alt: "product_pic" },
+        { id: 2, src: "https://i.imgur.com/Dfav1Yk.jpg", alt: "product_pic" },
+      ],
+      colors: [
+        { id: 1, hexcode: "#ffce30", selected: false },
+        { id: 2, hexcode: "#e83845", selected: false },
+        { id: 3, hexcode: "#e389b9", selected: false },
+        { id: 4, hexcode: "#746ab0", selected: false },
+        { id: 5, hexcode: "#288ba8", selected: true },
+      ],
+      sizes: [
+        { id: 1, name: "XS", selected: false },
+        { id: 2, name: "S", selected: false },
+        { id: 3, name: "M", selected: false },
+        { id: 4, name: "L", selected: false },
+        { id: 5, name: "XL", selected: false },
+        { id: 6, name: "2L", selected: true },
+      ],
+      quantity: 2,
+      unitPrice: 790,
+    },
+    {
+      id: 3,
+      pid: 5846,
+      name: "針織衫外套",
+      urls: [
+        { id: 1, src: "https://i.imgur.com/C7SYk0P.jpg", alt: "product_pic" },
+        { id: 2, src: "https://i.imgur.com/Dfav1Yk.jpg", alt: "product_pic" },
+      ],
+      colors: [
+        { id: 1, hexcode: "#ffce30", selected: false },
+        { id: 2, hexcode: "#e83845", selected: true },
+        { id: 3, hexcode: "#e389b9", selected: false },
+        { id: 4, hexcode: "#746ab0", selected: false },
+        { id: 5, hexcode: "#288ba8", selected: false },
+      ],
+      sizes: [
+        { id: 1, name: "XS", selected: false },
+        { id: 2, name: "S", selected: false },
+        { id: 3, name: "M", selected: false },
+        { id: 4, name: "L", selected: false },
+        { id: 5, name: "XL", selected: true },
+        { id: 6, name: "2L", selected: false },
+      ],
+      quantity: 4,
+      unitPrice: 1290,
+    },
+  ]);
+  // 模擬購物車是空的時候
+  // const [cartContext, setCartContext] = useState([]);
+  // 當 cartContext
+  const memorizedCart = useMemo(
+    () => ({ cartContext, setCartContext }),
+    [cartContext]
+  );
 
   return (
     <main>
       <Router>
         <Switch>
           <UserContext.Provider value={memorizedUser}>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <Route path="/profile-edit">
-              {/* 防止非用戶透過 url 存取編輯個人資訊頁面 */}
-              {user === null ? <Redirect to="/login" /> : <ProfileEditPage />}
-            </Route>
-            <Route path="/favorite">
-              {/* 防止非用戶透過 url 存取收藏清單頁面 */}
-              {user === null ? <Redirect to="/login" /> : <FavoritePage />}
-            </Route>
-            <Route path="/register">
-              <RegisterPage />
-            </Route>
-            <Route path="/products">
-              <ProductsPage />
-            </Route>
-            <Route path="/product">
-              <SingleProductPage />
-            </Route>
-            <Route path="/cart">
-              <CartPage />
-            </Route>
+            <CartContext.Provider value={memorizedCart}>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route path="/login">
+                {/* 防止已經是會員用戶透過 url 存取登入頁面 */}
+                {user !== null ? <Redirect to="/" /> : <LoginPage />}
+              </Route>
+              <Route path="/profile-edit">
+                {/* 防止非用戶透過 url 存取編輯個人資訊頁面 */}
+                {user === null ? <Redirect to="/login" /> : <ProfileEditPage />}
+              </Route>
+              <Route path="/favorite">
+                {/* 防止非用戶透過 url 存取收藏清單頁面 */}
+                {user === null ? <Redirect to="/login" /> : <FavoritePage />}
+              </Route>
+              <Route path="/register">
+                {/* 防止已經是會員用戶透過 url 存取註冊頁面 */}
+                {user !== null ? <Redirect to="/" /> : <RegisterPage />}
+              </Route>
+              <Route path="/products">
+                <ProductsPage />
+              </Route>
+              <Route path="/product">
+                <SingleProductPage />
+              </Route>
+              <Route path="/cart">
+                <CartPage />
+              </Route>
+            </CartContext.Provider>
           </UserContext.Provider>
         </Switch>
       </Router>
