be599f79bda93022f34a153aa5eea2c86cf17673
Emory <aa774281@gmail.com>
2021-12-11 18:49:52 +0800

    use initial value

--- src/context/index.js
+++ src/context/index.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-export const UserContext = createContext();
-export const CartContext = createContext();
-export const WatchedProductsContext = createContext();
-export const FavoriteItemsContext = createContext();
+export const UserContext = createContext({});
+export const CartContext = createContext([]);
+export const WatchedProductsContext = createContext([]);
+export const FavoriteItemsContext = createContext([]);
