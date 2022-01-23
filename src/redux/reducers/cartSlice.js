1153f6ac05e4365acec705b82fd098225c192904
Emory <aa774281@gmail.com>
2022-01-23 16:52:37 +0800

    use redux thunk correctly

--- src/redux/reducers/cartSlice.js
+++ src/redux/reducers/cartSlice.js
@@ -1,8 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getCartItemsApi } from "../../Webapi";
 import { getCookie, setCookie } from "../../util";
 
-const initialState = { items: [] };
+// state structure
+/* 
+  {
+    items: [
+      id  : number
+      pid : number
+      name: string
+      colors: [{
+        id      : number
+        hexcode :  string
+        selected: boolean
+      },...],
+      sizes: [{
+        id      : number
+        name    : string
+        selected: boolean
+      },...],
+      unitPrice : number,
+      quantity  : number
+    ],
+    err: {
+      isShow: boolean,
+      msg   : string
+    }
+  }
+*/
+
+const initialState = {
+  items: [],
+  err: {
+    isShow: false,
+    msg: "",
+  },
+};
+
+// redux-thunk
+export const getCart = createAsyncThunk("cart/getCart", async () => {
+  const resp = await getCartItemsApi();
+  return JSON.stringify(resp); // avoid 'non-serializable-data' warning
+});
 
 const cartSlice = createSlice({
   name: "cart",
@@ -12,52 +51,47 @@ const cartSlice = createSlice({
       state.items = action.payload;
     },
   },
-});
-
-// redux-thunk
-// 如果為用戶，透過 api 拿到購物車資訊
-// 如果為訪客，透過 cookie 拿購物車資訊
-export const getCart = () => (dispatch) => {
-  return new Promise((resolve, reject) => {
-    getCartItemsApi()
-      .then((resp) => {
-        const json_data = resp.data;
-        if (json_data.isSuccessful === "failed") {
-          // 如果是訪客，則從 cookie 拿購物車資訊
-          if (json_data.msg === "session variable not set") {
-            if (getCookie("cart-guest") === undefined) {
-              setCookie("cart-guest", JSON.stringify([]), 7);
-            } else {
-              dispatch(addCartItem(JSON.parse(getCookie("cart-guest"))));
-            }
-            // 如果用戶為訪客，則會觸發 failed 條件，所以加上 resolve() 解決
-            return resolve();
+  extraReducers: (builder) => {
+    builder.addCase(getCart.fulfilled, (state, action) => {
+      let parsed_json = null;
+      try {
+        parsed_json = JSON.parse(action.payload);
+      } catch {
+        state.err.isShow = true;
+        state.err.msg = "get response but parsed JSON failed";
+      }
+      if (parsed_json.data.isSuccessful === "failed") {
+        if (parsed_json.data.msg === "session variable not set") {
+          if (getCookie("cart-guest") === undefined) {
+            state.items = [];
+            setCookie("cart-guest", JSON.stringify([]), 7);
+          } else {
+            state.items = JSON.parse(getCookie("cart-guest"));
           }
-          return reject("server side error");
         }
-        if (json_data.isSuccessful === "successful") {
-          dispatch(
-            addCartItem(
-              json_data.data.map((item, index) => ({
-                id: index,
-                pid: item.pid,
-                name: item.name,
-                colors: JSON.parse(item.colors),
-                sizes: JSON.parse(item.sizes),
-                unitPrice: item.price,
-                urls: JSON.parse(item.imgs),
-                quantity: item.quantity,
-              }))
-            )
-          );
-          return resolve();
-        }
-      })
-      .catch((e) => {
-        return reject(e);
-      });
-  });
-};
+        state.err.isShow = true;
+        state.err.msg = "server side reject this operation";
+      }
+      if (parsed_json.data.isSuccessful === "successful") {
+        state.items = parsed_json.data.data.map((item, index) => ({
+          id: index,
+          pid: item.pid,
+          name: item.name,
+          colors: JSON.parse(item.colors),
+          sizes: JSON.parse(item.sizes),
+          unitPrice: item.price,
+          urls: JSON.parse(item.imgs),
+          quantity: item.quantity,
+        }));
+      }
+    });
+    builder.addCase(getCart.rejected, (state) => {
+      // console.log("rejected: ", action.error);
+      state.err.isShow = true;
+      state.err.msg = "send request failed";
+    });
+  },
+});
 
 export const { addCartItem } = cartSlice.actions;
 export default cartSlice.reducer;
