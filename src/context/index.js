f819f5eb74a23abcc4e7d353fb7cfa9efa9f2712
Emory <aa774281@gmail.com>
2022-01-26 17:01:25 +0800

    remove unused context

--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,2 @@
 import { createContext } from "react";
-export const UserContext = createContext({});
-export const FavoriteItemsContext = createContext([]);
 export const IntroductionModalContext = createContext(null);
