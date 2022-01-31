9cc203cffd8c12e09bac7f71d3bf99699ca0c597
Emory <aa774281@gmail.com>
2022-01-31 22:53:53 +0800

    add webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 // 用於設定 style-component babel 參數
 const rewireStyledComponents = require("react-app-rewire-styled-components");
+const rewireWebpackBundleAnalyzer = require("react-app-rewire-webpack-bundle-analyzer");
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
@@ -7,5 +8,11 @@ module.exports = function override(config, env) {
     // 開啟 styled-component 的模組名稱，方便 debug
     displayName: false,
   });
+  if (env === "production") {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: "server",
+      reportFilename: "report.html",
+    });
+  }
   return config;
 };
