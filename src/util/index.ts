15858b257c3b3f1d2cf1a043f89fb9ff332a0ee4
Emory <aa774281@gmail.com>
2022-02-19 15:08:49 +0800

    use ts syntax

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,24 +1,28 @@
 import { ROOT_DIR } from "../constant";
 // 根據 name 拿到相對應的 cookie 內容
 // 如果找不到則回傳 undefined
-export const getCookie = function (name) {
+export const getCookie = function (name: string): string | undefined {
   // 將開頭加上;，以便於 call split 分割
   const value = `; ${document.cookie}`;
   // 透過 '; <名稱>=' 特定字符切分
   // 如果存在不只一個 cookie，則應該會輸出 2 個字串的 array
-  const parts = value.split(`; ${name}=`);
+  const parts: string[] = value.split(`; ${name}=`);
   // 如果要找的名稱後面還有其他 cookie 名稱，則把不相干的刪掉
-  if (parts.length === 2)
+  if (parts.length === 2) {
     // 如果遇到中文，使用此編碼解決 Safari 上無法在 cookie 上儲存的問題
-    return decodeURIComponent(parts.pop().split(";").shift());
+    // 使用型別斷言，保證一定是 string
+    let value = parts.pop() as string;
+    return decodeURIComponent(value.split(";").shift() as string);
+  }
+  return undefined;
 };
 
 // 根據傳入參數，設定相對應的 cookie
 export const setCookie = function (
-  name,
-  value,
-  expireDays = 1,
-  path = ROOT_DIR
+  name: string,
+  value: string,
+  expireDays: number = 1,
+  path: string = ROOT_DIR
 ) {
   const now = new Date();
   now.setDate(now.getDate() + expireDays);
@@ -29,7 +33,7 @@ export const setCookie = function (
 };
 
 // 空物件，回傳 true，否則回傳 false
-export const isEmptyObj = function (obj) {
+export const isEmptyObj = function (obj: object): boolean {
   if (
     obj &&
     Object.keys(obj).length === 0 &&
